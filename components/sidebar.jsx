24d4c6ae6c88bafb8ee17fbd079bde3e74ee8a9c
Oscar Guerra <oscarluisgm2@gmail.com>
2024-09-12 18:10:12 -0400

    ok

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,7 +1,7 @@
 import { Sidebar} from 'primereact/sidebar';
 import { Button } from 'primereact/button';       
 import { useState } from 'react';
-        
+     
 
 const SidebarComponent=()=>{
     const [visible, setVisible]=useState(false)
@@ -9,14 +9,15 @@ const SidebarComponent=()=>{
 
 
     return <>
-    <Sidebar visible={visible} onHide={() => setVisible(false)} /* icons={customIcons} */>
+    
+    <Sidebar visible={visible} position='right' onHide={() => setVisible(false)} /* icons={customIcons} */ >
     <h2>Sidebar</h2>
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
     </p>
     </Sidebar>
-    {!visible && <Button icon="pi pi-plus" onClick={() => setVisible(true)}>ok</Button>}
+    {!visible && <Button label='Boton' icon="pi pi-check" onClick={() => setVisible(true)}/>}
 
     </>
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { PrimeReactProvider} from 'primereact/api';
+import 'primeicons/primeicons.css'
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
         
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <PrimeReactProvider>
+  <PrimeReactProvider value={{ unstyled: false }}>
     <App />
   </PrimeReactProvider>
 )
